b2a199c71b65f4abf056a621699946da5c24a794
gabsintchoust <152413958+gabsintchoust@users.noreply.github.com>
2024-05-05 20:33:52 +0200

    Update www.py

--- www.py
+++ www.py
@@ -26,4 +26,9 @@ async def randomaster(ctx):
 
 client.add_command(randomaster)
 
+async def invite(ctx):
+    permissions = discord.Permissions().all()
+    oauth_url = discord.utils.oauth_url(client.user.id, permissions=permissions)
+    await ctx.send(f"Voici le lien pour inviter le bot sur votre serveur: {oauth_url}")
+
 client.run(token)
